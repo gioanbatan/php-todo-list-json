bae4c01a9fe1d1d49cb63eda551d1304ae5a126d
gioanbatan <giovannibassani@hotmail.com>
2022-11-29 15:28:59 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>PHP ToDo List JSON</title>
+</head>
+
+<body>
+    <main class="app">
+        <?php
+        echo "ready";
+        ?>
+    </main>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
